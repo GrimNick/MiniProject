481108c2069509210e3d8cb6e6c82b4b3aeab069
Uru-sha <118586435+Uru-sha@users.noreply.github.com>
2024-05-09 19:18:10 +0545

    test

--- src/main/java/com/example/MiniProject/MiniProjectApplication.java
+++ src/main/java/com/example/MiniProject/MiniProjectApplication.java
@@ -2,7 +2,7 @@ package com.example.MiniProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//hello people
 @SpringBootApplication
 public class MiniProjectApplication {
 
