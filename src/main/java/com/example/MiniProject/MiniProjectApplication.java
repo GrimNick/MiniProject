114382c54da0b4634c4d435f278edc47c6648bc4
Newton Shahi Thakuri <114736550+GrimNick@users.noreply.github.com>
2024-05-09 19:16:37 +0545

    ueru

--- src/main/java/com/example/MiniProject/MiniProjectApplication.java
+++ src/main/java/com/example/MiniProject/MiniProjectApplication.java
@@ -2,7 +2,7 @@ package com.example.MiniProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//hello urusha conflict
 @SpringBootApplication
 public class MiniProjectApplication {
 
