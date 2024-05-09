1f82ada69e1da1b032a7aa0fa421a70a8ea3a865
Sabin Kaphle <iamsabin35@gmail.com>
2024-05-09 19:52:58 +0545

    hello

--- src/main/java/com/example/MiniProject/MiniProjectApplication.java
+++ src/main/java/com/example/MiniProject/MiniProjectApplication.java
@@ -2,7 +2,6 @@ package com.example.MiniProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//hello sagar
 @SpringBootApplication
 public class MiniProjectApplication {
 
