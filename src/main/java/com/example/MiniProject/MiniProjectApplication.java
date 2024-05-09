602f7e6c6d40eed44a01f16b73fffca000b86550
Sagar Adhikari <118412981+sagarad23@users.noreply.github.com>
2024-05-09 20:04:17 +0545

    Update MiniProjectApplication.java

--- src/main/java/com/example/MiniProject/MiniProjectApplication.java
+++ src/main/java/com/example/MiniProject/MiniProjectApplication.java
@@ -2,9 +2,7 @@ package com.example.MiniProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//hello sagar
-//hello urusha
-//k xa halkhabar
+
 @SpringBootApplication
 public class MiniProjectApplication {
 
