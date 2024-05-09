d7326f2cfb4138cb8631ccdfefcd5385ae1377d2
Newton Shahi Thakuri <114736550+GrimNick@users.noreply.github.com>
2024-05-09 19:04:22 +0545

    test fixed

--- src/main/java/com/example/MiniProject/MiniProjectApplication.java
+++ src/main/java/com/example/MiniProject/MiniProjectApplication.java
@@ -2,7 +2,7 @@ package com.example.MiniProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//hello sagar
 @SpringBootApplication
 public class MiniProjectApplication {
 
